91d2bdd31f3a3d6edfed5f6f89ab8f5c4d20d715
ehonlia <hongxin.liang@ericsson.com>
2011-02-28 17:53:27 +0800

    edit now works

--- src/main/scala/com/honnix/myourls/snippet/Admin.scala
+++ src/main/scala/com/honnix/myourls/snippet/Admin.scala
@@ -25,9 +25,11 @@ class Admin extends Loggable {
     ShortenedUrl.createRecord.date(new Date)
   )
 
-  private val DisplayIdPrefix = "id-"
+  private val IdPrefix = "id-"
 
-  private val EditIdPrefix = "edit-"
+  private val EditPrefix = "edit-"
+
+  private val DeletePrefix = "delete-"
 
   private var currentShortenedUrl: ShortenedUrl = _
 
@@ -42,51 +44,46 @@ class Admin extends Loggable {
                onclick={deferCall(Str(name + "=true"), jsFunc).toJsCmd + "; return false;"}/>))(_ % _)
   }
 
-  private def ajaxInputButton(text: NodeSeq, func: () => JsCmd, attrs: MetaData*): Elem = {
-    attrs.foldLeft(fmapFunc(contextFuncBuilder(func))(name =>
-        <input type="button" value={text}
-               onclick={makeAjaxCall(Str(name + "=true")).toJsCmd + "; return false;"}/>))(_ % _)
-  }
-
   private def generateRow(shortenedUrl: ShortenedUrl) = {
     def delete(record: ShortenedUrl) = {
       record.delete_!
-      new FadeOut(record.id.toString, 0 second, 1 second)
+      FadeOut(record.id.toString, 0 second, 1 second)
     }
 
     def edit(record: ShortenedUrl) = {
       def update(originalUrl: String) = {
         record.originUrl(originalUrl).save
-        _Noop
+        Call("postsave", record.linkId.value)
       }
 
-      val urlId = "edit-url-" + shortenedUrl.linkId.value
-      val (name, js) = ajaxCall(ValById(urlId), update)
+      val (name, js) = ajaxCall(Call("presave", record.linkId.value), update)
+      val urlId = EditPrefix + "url-" + shortenedUrl.linkId.value
 
-      val tr = <tr id={EditIdPrefix + shortenedUrl.linkId.value} class="edit-row">
+      val tr = <tr id={EditPrefix + shortenedUrl.linkId.value} class="edit-row">
         <td colspan="6">
-          Edit: <strong>original URL</strong>
+          Edit:
+          <strong>original URL</strong>
           :
-          <input type="text" id={urlId} name="edit-url" value={shortenedUrl.originUrl.value}
+          <input type="text" id={urlId} name={urlId} value={shortenedUrl.originUrl.value}
                  class="text" size="100"></input>
         </td>
         <td colspan="1">
-          <input type="button" title="Save new value" value="Save" class="button" onclick={js.toJsCmd}></input>
-          <input type="button" title="Cancel editing" value="X" class="button"
+          <input type="button" id={EditPrefix + "submit-" + shortenedUrl.linkId.value} title="Save new value"
+                 value="Save" class="button" onclick={js.toJsCmd}></input>
+          <input type="button" id={EditPrefix + "close-" + shortenedUrl.linkId.value} title="Cancel editing" value="X" class="button"
                  onclick={"hide_edit('" + shortenedUrl.linkId.value + "')"}></input>
         </td>
       </tr>
 
-      val func = JsCrVar("func", Jx(tr).toJs)
-      func & Jq(Call("func", "document") ~> JsVal("firstChild")) ~>
-              JsFunc("insertAfter", "#" + DisplayIdPrefix + shortenedUrl.linkId.value)
+      JsCrVar("func", Jx(tr).toJs) & Jq(Call("func", "document") ~> JsVal("firstChild")) ~>
+              JsFunc("insertAfter", "#" + IdPrefix + shortenedUrl.linkId.value) & Focus(urlId)
     }
 
-    <tr id={DisplayIdPrefix + shortenedUrl.linkId.value}>
+    <tr id={IdPrefix + shortenedUrl.linkId.value}>
       <td>
         {shortenedUrl.linkId.value}
       </td>
-      <td>
+      <td id={"url-" + shortenedUrl.linkId.value}>
         <a href={shortenedUrl.originUrl.value}>
           {shortenedUrl.originUrl.value}
         </a>
@@ -106,22 +103,23 @@ class Admin extends Loggable {
         {shortenedUrl.clickCount.value}
       </td>
       <td class="actions">
-        {ajaxInputButton(Text("Edit"), () => edit(shortenedUrl), "class" -> "button") ++ Text(" ") ++
-              ajaxInputButton(Text("Del"), Call("remove"), () => delete(shortenedUrl), "class" -> "button")}
+        {ajaxInputButton(Text("Edit"), Call("edit", shortenedUrl.linkId.value), () => edit(shortenedUrl),
+        "class" -> "button", "id" -> (EditPrefix + "button-" + shortenedUrl.linkId.value)) ++ Text(" ") ++
+              ajaxInputButton(Text("Del"), Call("remove"), () => delete(shortenedUrl),
+                "class" -> "button", "id" -> (DeletePrefix + "button-" + shortenedUrl.linkId.value))}
       </td>
     </tr>
   }
 
   def add = {
-
     def save = {
       val shortenedUrl = DependencyFactory.inject[ShortenedUrlMetaRecord].open_!
 
       import net.liftweb.json.JsonDSL._
 
-      val cmd = if (shortenedUrl.find(ShortenedUrl.originUrl.name -> currentShortenedUrl.originUrl.value).isDefined) {
+      if (shortenedUrl.find(ShortenedUrl.originUrl.name -> currentShortenedUrl.originUrl.value).isDefined) {
         notice(currentShortenedUrl.originUrl.value + " already exists in database")
-        _Noop
+        Call("restore_add_button").cmd
       }
       else {
         val linkId = (DependencyFactory.inject[NextIdGenerator].open_! !? 'id).toString
@@ -129,21 +127,14 @@ class Admin extends Loggable {
                 ip(containerRequest.open_!.remoteAddress).clickCount(0).save
         notice(currentShortenedUrl.originUrl.value + " added to database")
         PrependHtml("tblUrl-body", generateRow(currentShortenedUrl)) & Hide(currentShortenedUrl.id.toString) &
-                FadeIn(currentShortenedUrl.id.toString, 0 second, 1 second) & Call("reset_url") & Call("zebra_table") &
-                Call("increment")
-        //        val func = JsCrVar("func", Jx(generateRow(currentShortenedUrl)).toJs)
-        //        func & Jq(Call("func", "document") ~> JsVal("firstChild")) ~> JsFunc("prependTo", "#tblUrl-body") ~>
-        //                JsFunc("hide") ~> JsFunc("fadeIn", "1000")
+                FadeIn(currentShortenedUrl.id.toString, 0 second, 1 second) & Call("postadd")
       }
-
-      cmd & Call("end_loading", "#add-button") &
-              Call("end_disable", "#add-button")
     }
 
     "#add-url" #> text("http://", currentShortenedUrlVar.originUrl(_)) &
             "#current" #> hidden(() => currentShortenedUrl = currentShortenedUrlVar.is) &
             "#add-button" #> ajaxSubmit("Shorten The URL", save _) andThen {
-      "form" #> ((ns: NodeSeq) => ajaxForm(JsIf(JsEq(Call("validate"), JsFalse), JsReturn(false)), ns))
+      "form" #> ((ns: NodeSeq) => ajaxForm(JsIf(JsEq(Call("preadd"), JsFalse), JsReturn(false)), ns))
     }
   }
 
--- src/main/webapp/js/insert.js
+++ src/main/webapp/js/insert.js
@@ -1,114 +1,90 @@
-// Init some stuff
 $(document).ready(function(){
-	reset_url();
-	if ($("#tblUrl tr.nourl_found").length != 1) {
-		$("#tblUrl").tablesorter({
-			sortList:[[3,1]], // Sort on column #3 (numbering starts at 0)
-			headers: { 6: {sorter: false} }, // no sorter on column #6
-			widgets: ['zebra'] // prettify
-		});
-	}
+    reset_url();
+    if ($("#tblUrl tr.nourl_found").length != 1) {
+        $("#tblUrl").tablesorter({
+            sortList:[[3,1]], // Sort on column #3 (numbering starts at 0)
+            headers: { 6: {sorter: false} }, // no sorter on column #6
+            widgets: ["zebra"] // prettify
+        });
+    }
 });
 
-// Before submitting form, do validation
-function validate() {
-	var newurl = $("#add-url").val();
-	if ( !newurl || newurl == 'http://' || newurl == 'https://' ) {
-		alert('no URL ?');
-		return false;
-	}
-	add_loading("#add-button");
-	return true;
+function preadd() {
+    var newurl = $("#add-url").val();
+    if ( !newurl || newurl == "http://" || newurl == "https://" ) {
+        alert("no URL ?");
+        return false;
+    }
+    add_loading("#add-button");
+    return true;
 }
 
-// Display the edition interface
-function edit(id) {
-	add_loading("#edit-button-" + id);
-	add_loading("#delete-button-" + id);
-	$.getJSON(
-		"index_ajax.php",
-		{ mode: "edit_display", id: id },
-		function(data){
-			$("#id-" + id).after( data.html );
-			$("#edit-url-"+ id).focus();
-			end_loading("#edit-button-" + id);
-			end_loading("#delete-button-" + id);
-		}
-	);
+function postadd() {
+    reset_url();
+    zebra_table();
+    increment();
+    restore_add_button();
 }
 
-// Delete a link
-function remove(realDelete) {
-	if (confirm('Really delete?')) {
-		realDelete();
-	}
+function restore_add_button() {
+    remove_loading("#add-button");
 }
 
-// Cancel edition of a link
-function hide_edit(id) {
-	$("#edit-" + id).fadeOut(200, function(){
-		end_disable("#edit-button-" + id);
-		end_disable("#delete-button-" + id);
-	});
-}
-
-// Save edition of a link
-function edit_save(id) {
-	add_loading("#edit-close-" + id);
-	var newurl = $("#edit-url-" + id).val();
-	var newid = $("#edit-id-" + id).val();
-	$.getJSON(
-		"index_ajax.php",
-		{mode:'edit_save', url: newurl, id: id, newid: newid },
-		function(data){
-			if(data.status == 'success') {
-				$("#url-" + id).html('<a href="' + data.url.url + '" title="' + data.url.url + '">' + data.url.url + '</a>');
-				$("#keyword-" + id).html(data.url.keyword);
-				$("#shorturl-" + id).html('<a href="' + data.url.shorturl + '" title="' + data.url.shorturl + '">' + data.url.shorturl + '</a>');
-				$("#timestamp-" + id).html(data.url.date);
-				$("#edit-" + id).fadeOut(200, function(){
-					$('#tblUrl tbody').trigger("update");
-				});
-			}
-			feedback(data.message, data.status);
-			end_disable("#edit-close-" + id);
-			end_loading("#edit-close-" + id);
-			end_disable("#edit-button-" + id);
-			end_disable("#delete-button-" + id);
-		}
-	);
-}
-
-// Begin the spinning animation & disable a button
 function add_loading(el) {
-	$(el).attr("disabled", "disabled").addClass('disabled').addClass('loading');
+    $(el).attr("disabled", "disabled").addClass("disabled").addClass("loading");
 }
 
-// End spinning animation
-function end_loading(el) {
-	$(el).removeClass('loading');
+function remove_loading(el) {
+    $(el).removeClass("loading").removeAttr("disabled").removeClass("disabled");
 }
 
-// Un-disable an element
-function end_disable(el) {
-	$(el).removeAttr("disabled").removeClass('disabled');
-}
-
-// Prettify table with odd & even rows
 function zebra_table() {
-	$("#tblUrl tbody tr:even").removeClass('odd').addClass('even');
-	$("#tblUrl tbody tr:odd").removeClass('even').addClass('odd');
-	$('#tblUrl tbody').trigger("update");
+    $("#tblUrl tbody tr:even").removeClass("odd").addClass("even");
+    $("#tblUrl tbody tr:odd").removeClass("even").addClass("odd");
+    $("#tblUrl tbody").trigger("update");
 }
 
-// Ready to add another URL
 function reset_url() {
-	$('#add-url').val('http://').focus();
+    $("#add-url").val("http://").focus();
 }
 
-// Increment URL counters
 function increment() {
-	$('.increment').each(function(){
-		$(this).html( parseInt($(this).html()) + 1);
-	});
+    $(".increment").each(function(){
+        $(this).html( parseInt($(this).html()) + 1);
+    });
+}
+
+function edit(id, realEdit) {
+    add_loading("#edit-button-" + id);
+    add_loading("#delete-button-" + id);
+    realEdit();
+    $("#edit-button-" + id).removeClass("loading");
+    $("#delete-button-" + id).removeClass("loading");
+}
+
+function remove(realDelete) {
+    if (confirm("Really delete?")) {
+        realDelete();
+    }
+}
+
+function hide_edit(id) {
+    $("#edit-" + id).fadeOut(200, function(){
+        remove_loading("#edit-button-" + id);
+        remove_loading("#delete-button-" + id);
+        $(this).remove();
+    });
+}
+
+function presave(id) {
+    add_loading("#edit-close-" + id);
+    return $("#edit-url-" + id).val();
+}
+
+function postsave(id) {
+    var url = $("#edit-url-" + id).val();
+    $("#url-" + id).html('<a href="' + url + '">' + url + '</a>');
+    remove_loading("#edit-close-" + id);
+    remove_loading("#delete-button-" + id);
+    hide_edit(id);
 }
